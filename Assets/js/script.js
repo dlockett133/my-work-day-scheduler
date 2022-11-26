82463b081b6f3ce18ddf45e92424757db7ab8789
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-11-25 22:56:51 -0500

    modified styling selectors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,3 +5,16 @@ var currentDayEl = $("#currentDay");
 
 // Inserts today's date in currentDayEl element in (Monday, January 1st) format 
 currentDayEl.text(`${today.format("dddd, MMMM Do")}`);
+
+
+/* <div class="time-block">
+        <form class="row">
+            <p class="hour">9am</p>
+            <textarea class="description present" style="
+    flex: 1;
+"></textarea>
+            <button class="saveBtn" type="submit" style="
+">Save the date</button>
+        </form>
+    </div>
+    </div> */
