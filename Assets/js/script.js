0d7bb9400e7c43ed711425182e5b99405393e753
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-11-25 23:43:23 -0500

    Appended timeBlockEl to the container

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,12 +2,14 @@ var today = moment(); // Returns the date/time for the present day
 
 // Selects 'currentDay' ID Element
 var currentDayEl = $("#currentDay");
-
-// Creates a 'div' element with a 'time-block' class attribute
-var timeBlockEl = $("<div>").addClass("time-block")
 // Inserts today's date in currentDayEl element in (Monday, January 1st) format 
 currentDayEl.text(`${today.format("dddd, MMMM Do")}`);
 
+// Creates a 'div' element with a 'time-block' class attribute
+var timeBlockEl = $("<div>").addClass("time-block")
+
+// Appends element to the end of the 'container' class element
+timeBlockEl.appendTo(".container")
 
 /* <div class="time-block">
         <form class="row">
